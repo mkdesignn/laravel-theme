3dc22fa063eb686b5d6719e9e4b660487234f834
mkdesignn <mohammad.kaab@gmail.com>
2017-10-06 16:31:38 +0330

    add fonts and fix the 404 page text

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
     <main class="container content-wrap">
         <!-- col-lg-9 col-md-9 col-sm-8 -->
         <section class="blog-posts">
-           <h3 class="center"> صحفه مورد نظر یافت نشد  </h3>
+           <h3 class="center"> صفحه مورد نظر یافت نشد  </h3>
         </section>
     </main>
 @endsection
